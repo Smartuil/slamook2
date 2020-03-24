5c3bbb72311552a77a10c4dba4f8ab3cd16601d0
smartuil <563412673@qq.com>
2020-03-24 15:23:54 +0800

    ch10

---
+++ ch10/cmake-build-debug/CMakeFiles/ch10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ch10.dir/main.cpp.o"
+  "ch10"
+  "ch10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
