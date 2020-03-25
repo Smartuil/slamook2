0f1fb16efd83560b31031759ab2d446b70371a2c
smartuil <563412673@qq.com>
2020-03-25 15:02:36 +0800

    ch12

---
+++ ch12/cmake-build-debug/CMakeFiles/ch12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ch12.dir/main.cpp.o"
+  "ch12"
+  "ch12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ch12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ch12/cmake-build-debug/dense_mono/CMakeFiles/dense_mapping.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/smartuil/CLionProjects/slambook2/ch12/dense_mono/dense_mapping.cpp" "/home/smartuil/CLionProjects/slambook2/ch12/cmake-build-debug/dense_mono/CMakeFiles/dense_mapping.dir/dense_mapping.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "/usr/local/lib/cmake/Sophus/../../../include"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
